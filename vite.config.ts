a351a8407b089e63bb41451a60c7feadf2b2f66a
mil <1363944642@qq.com>
2023-10-11 12:10:57 +0800

    开启rollup-plugin-visualizer 分析页面

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
 import { defineConfig } from 'vite';
+import visualizer from 'rollup-plugin-visualizer';
+
 export default defineConfig({
+  plugins: [
+    visualizer({ open: true }), // 自动开启分析页面
+  ],
+
   build: {
     lib: {
       entry: 'src/index.ts',
       name: 'color-extraction',
     },
-    rollupOptions: {
-      external: ['colorthief', 'nearest-color', 'colord'],
-      output: {
-        globals: {
-          'nearest-color': 'nearest-color',
-          colorthief: 'colorthief',
-          colord: 'colord',
-        },
-      },
-    },
+    // rollupOptions: {
+    //   external: ['colorthief', 'nearest-color', 'colord'],
+    //   output: {
+    //     globals: {
+    //       'nearest-color': 'nearest-color',
+    //       colorthief: 'colorthief',
+    //       colord: 'colord',
+    //     },
+    //   },
+    // },
   },
 });
